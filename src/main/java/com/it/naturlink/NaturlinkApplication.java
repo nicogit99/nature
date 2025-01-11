ae110729128551395b234b73fea34ea72695b08c
nicogit99 <nicoml1804@gmail.com>
2025-01-11 10:18:15 +0100

    modifiche 4

---
+++ src/main/java/com/it/naturlink/NaturlinkApplication.java
@@ -0,0 +1,20 @@
+package com.it.naturlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableJpaRepositories(basePackages = "com.it.naturlink.repository")
+@EntityScan(basePackages = "com.it.naturlink.db")
+
+public class NaturlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturlinkApplication.class, args);
+	}
+
+}
