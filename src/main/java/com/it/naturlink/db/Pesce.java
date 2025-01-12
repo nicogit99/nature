5dee12b024b116cd8df546ee7b6c50985f6637fa
nicogit99 <nicoml1804@gmail.com>
2025-01-12 12:12:51 +0100

    modofiche 5

---
+++ src/main/java/com/it/naturlink/db/Pesce.java
@@ -0,0 +1,13 @@
+package com.it.naturlink.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Pesce {
+    private String tipoPesce;
+    private double quantitaAnnuale;
+}
---
+++ src/main/java/com/it/naturlink/db/Sivicolture.java
@@ -0,0 +1,20 @@
+package com.it.naturlink.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "SIVICOLTURA")
+@Entity
+@NoArgsConstructor
+public class Sivicolture extends  {
+
+
+
+
+}
